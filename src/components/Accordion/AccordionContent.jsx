4016945857e7106ffa69c6358c12d97ad661a208
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-25 14:16:30 +0700

    Sharing Cross-Component State When Working With Compound Components

--- src/components/Accordion/AccordionContent.jsx
+++ src/components/Accordion/AccordionContent.jsx
@@ -1,12 +1,19 @@
 import { useAccordionContext } from "./Accordion";
+import { useAccordionItemContext } from "./AccordionItem";
 
-export default function AccordionContext({ id, className, children }) {
+export default function AccordionContext({ className, children }) {
+  const id = useAccordionItemContext();
+
   const { openItemId } = useAccordionContext();
 
   const isOpen = openItemId === id;
 
   return (
-    <div className={isOpen ? `${className ?? ""} open` : `${className ?? ""} close`}>
+    <div
+      className={
+        isOpen ? `${className ?? ""} open` : `${className ?? ""} close`
+      }
+    >
       {children}
     </div>
   );
