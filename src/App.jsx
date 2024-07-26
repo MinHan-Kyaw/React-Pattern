8db4f89bb59b0f63f45a61e3128231f1534f13d0
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-07-26 14:54:35 +0700

    Implementing a Search Functionality With Help Of Render Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Accordion from "./components/Accordion/Accordion";
 import SearchableList from "./components/SearchableList/SearchableList";
 
-import savannaImg from './assets/african-savanna.jpg';
-import amazonImg from './assets/amazon-river.jpg';
-import caribbeanImg from './assets/caribbean-beach.jpg';
-import desertImg from './assets/desert-dunes.jpg';
-import forestImg from './assets/forest-waterfall.jpg';
-
+import savannaImg from "./assets/african-savanna.jpg";
+import amazonImg from "./assets/amazon-river.jpg";
+import caribbeanImg from "./assets/caribbean-beach.jpg";
+import desertImg from "./assets/desert-dunes.jpg";
+import forestImg from "./assets/forest-waterfall.jpg";
+import Place from "./Place";
 
 const PLACES = [
   {
@@ -78,8 +78,12 @@ function App() {
         </Accordion>
       </section>
       <section>
-        <SearchableList items={PLACES} />
-        <SearchableList items={["Item 1", "Item 2"]} />
+        <SearchableList items={PLACES}>
+          {(item) => <Place item={item} />}
+        </SearchableList>
+        <SearchableList items={["Item 1", "Item 2"]}>
+          {(item) => item}
+        </SearchableList>
       </section>
     </main>
   );
